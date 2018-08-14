d44e8382b10266589363809a9532644ed6b7478f
mcryan2 <ryancmaya@gmail.com>
2018-08-14 19:06:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Maya"
+  end
+
+  get '/hometown' do 
+    "My hometown is Chicago"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Oh, Jeez"
+  end
 end
